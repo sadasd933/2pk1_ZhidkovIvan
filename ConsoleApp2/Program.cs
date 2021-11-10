693d3d2ffe559eafb48aa3b1c7379f903eaf34ad
ivan zhidkov <ivanzhidkov2005@mail.ru>
2021-11-10 18:06:37 +0500

    pz_10(super finale)

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Sample
+{
+    public static void Main()
+    {
+        char b = 'B', c = '\x63', d = '\u0032';
+        Console.WriteLine("{0} {1} {2}", b, c, d);
+        Console.WriteLine("{0} {1} {2}", char.ToLower(b), char.ToUpper(c), char.GetNumericValue(d));
+        char a;
+        do
+        {
+            Console.Write("Введите символ: ");
+            a = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введен символ {0}, его код - {1}", a, (int)a);
+            if (char.IsLetter(a)) Console.WriteLine("Буква ");
+            if (char.IsUpper(a)) Console.WriteLine("Верхний регистр");
+            if (char.IsLower(a)) Console.WriteLine("Нижний регистр");
+            if (char.IsControl(a)) Console.WriteLine("Управляющий");
+            if (char.IsNumber(a)) Console.WriteLine("Число ");
+            if (char.IsPunctuation(a)) Console.WriteLine("Разделитель");
+            Console.WriteLine("\n");
+        }
+        while (a != 'q');
+    }   
+}
--- pz_10/Program.cs
+++ pz_10/Program.cs
@@ -9,66 +9,68 @@ namespace pz_10
     {
         static void Main(string[] args)
         {
-            int[] maxindex = new int[9];
-            char[] max = new char[9];
 
-            char[][] arr = new char[9][];
-            
+            int[] maxindex = new int[72];
+            int[] maxnx = new int[72];
+            int[][] arr = new int[9][];
+
             Console.WriteLine("Ступенчатый массив:");
             for (int a = 0; a < 9; a++)
             {
-                arr[a] = new char[r.Next(10, 35)];
+                arr[a] = new int[r.Next(10, 35)];
                 for (int b = 0; b < arr[a].Length; b++)
                 {
-                    var c = GetRandomChar();
+                    int c = r.Next(65, 91);
                     arr[a][b] = c;
-                    Console.Write(c + " ");
+                    Console.Write(Convert.ToChar(c) + " ");
                 }
                 Console.WriteLine();
             }
             Console.WriteLine();
-            
+
             Console.WriteLine("Одномерный массив с последними элементами каждой строки: ");
             for (int c = 0; c< arr.Length; c++)
             {
-                var last = arr[c].Last();
-                Console.Write(last + " ");
+                int last = arr[c].Last();
+                Console.Write(Convert.ToChar(last) + " ");
             }
             Console.WriteLine("\n");
+
+            maxnx[0] = -1;
 
             Console.WriteLine("Одномерный массив с максимальными элементами каждой строки: ");
 
-            foreach (char[] z in arr)
+            for (int i = 0; i < 9; i++)
             {
-                for (int i = 0; i < 9; i++)
+                for (int x = 0; x < arr[i].Length; x++)
                 {
-                    for (int x = 0; x < arr[i].Length; x++)
+                    if (maxnx[i] <= arr[i][x])
                     {
-                        int qq = z[0].CompareTo(arr[i][x]);
-                        if (x == arr[i].Length - 1)
-                        {
-                            if (qq > 0)
-                            {
-                                Console.Write(z[0] + " ");
-                                max[i] = z[0];
-                                z[0] = arr[i][0];
-                                qq = z[0].CompareTo(arr[i][x]);
-                                break;
-                            }
-                        }
-                        if (qq < 0)
-                        {
-                            z[0] = arr[i][x];
-                            qq = z[0].CompareTo(arr[i][x]);
-                        }
-                        
+                        maxnx[i] = arr[i][x];
+                        maxindex[i] = x;
                     }
+                    if (x >= arr[i].Length - 1)
+                    {
+                        Console.Write(Convert.ToChar(maxnx[i]) + " ");
+                        break;
+                    }
+
                 }
-                break;
             }
             Console.WriteLine("\n");
-            Console.WriteLine("Ступенчатый массив с изменёнными элементами присутствовать не будет потому что у меня уже мозг плавится");
-            
+
+            Console.WriteLine("Ступенчатый массив с изменёнными элементами:");
+            for (int i = 0; i < 9; i++)
+            {
+                for (int x = 0; x < arr[i].Length; x++)
+                {
+                        var tmp = maxindex[i];
+                        arr[i][0] = maxnx[i];
+                        arr[i][tmp] = arr[i][0];
+                        Console.Write(Convert.ToChar(arr[i][x]) + " ");
+                }
+                Console.WriteLine();
+            }
         }
 
 
