80e83c650d275712347f14ea6c58a8a5b7520719
ivan zhidkov <ivanzhidkov2005@mail.ru>
2021-11-10 18:22:03 +0500

    pz_11(ez)

---
+++ pz_11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+
+namespace pz_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите несколько слов через пробел: ");
+            string input = Console.ReadLine();
+            string[] str = input.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < str.Length; i++)
+            {
+                var tmp = str[0];
+                str[0] = str[str.Length -1];
+                str[str.Length - 1] = tmp;
+                Console.Write(str[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
