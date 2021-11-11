a072035dc1a5f5428114ea211aa4ce8f1f7dde27
qnexc <74954462+sadasd933@users.noreply.github.com>
2021-11-11 06:58:32 +0300

    Add files via upload

---
+++ pz_12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pz_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+            char simv1 = 'a';
+            char simv2 = 'b';
+
+            if (text.Length % 2 == 1) 
+            {
+                simv1 = text[(text.Length - 1) / 2]; 
+                Console.WriteLine(simv1);
+            }
+            else if (text.Length % 2 == 0) 
+            {
+                simv1 = text[text.Length / 2]; 
+                simv2 = text[(text.Length / 2) - 1 ];  
+                Console.WriteLine($"{simv1}{simv2}");
+            }
+        }
+    }
+}
