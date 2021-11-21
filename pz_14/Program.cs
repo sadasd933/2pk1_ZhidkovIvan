4ad711c750c93336beb7d700597706a50afe4e88
ivan zhidkov <ivanzhidkov2005@mail.ru>
2021-11-21 15:21:15 +0500

    pz_14

---
+++ pz_14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace pz_14
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToInput = @"C:\Users\ivanz\OneDrive\Рабочий стол\input.txt";
+            string pathToOutput = @"C:\Users\ivanz\OneDrive\Рабочий стол\output.txt";
+            int[] nums = new int[100];
+            File.Delete(pathToInput);
+
+            using (StreamWriter sw = File.AppendText(pathToInput))
+            {
+                Random rand = new Random();
+                for (int i = 0; i < 100; i++)
+                {
+                    nums[i] = rand.Next(0, 101);
+                    sw.WriteLine(nums[i]);
+                }
+                Console.WriteLine("Числа в файле input.txt сгенерированы.");
+            }
+            var a = nums.Max();
+            var b = nums.Min();
+            using (StreamWriter sw = File.AppendText(pathToOutput))
+            {
+                sw.WriteLine(b);
+                sw.WriteLine(a);
+            }
+            Console.WriteLine($"Максимальное({a}) и минимальное({b}) числа записаны в файл output.txt.");
+        }
+    }
+}
